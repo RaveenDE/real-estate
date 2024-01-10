be3d3e060780380a3664ce6e4a5e813a0428becf
RaveenDE <rdesilva614@gmail.com>
2024-01-10 23:24:09 +0530

    connect to DB

--- api/index.js
+++ api/index.js
@@ -1,4 +1,13 @@
 import express from 'express';
+import mongoose from 'mongoose';
+
+mongoose.connect("mongodb+srv://raveen:1234@cluster0.hy2vc2c.mongodb.net/?retryWrites=true&w=majority")
+.then(()=>{
+    console.log('Connected to MONGODB!');
+}).catch((err) =>{
+    console.log(err);
+})
+
 const app = express();
 app.listen(3000, () => {
 console.log('Server is running on port 3000!!!');
