7207d6d1d5fa74b645c4a81cb5faf1e631960ea7
RaveenDE <rdesilva614@gmail.com>
2024-01-15 13:09:58 +0530

    Create signup API route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import userRouter from './routes/user.routes.js';
+import authRouter from './routes/auth.routes.js';
 
 mongoose.connect("mongodb+srv://raveen:1234@cluster0.hy2vc2c.mongodb.net/?retryWrites=true&w=majority")
 .then(()=>{
@@ -9,10 +10,15 @@ mongoose.connect("mongodb+srv://raveen:1234@cluster0.hy2vc2c.mongodb.net/?retryW
     console.log(err);
 })
 
+
 const app = express();
+
+app.use(express.json());
+
 app.listen(3000, () => {
 console.log('Server is running on port 3000!!!');
 }
 );
 
 app.use('/api/user',userRouter);
+app.use('/api/auth',authRouter);
